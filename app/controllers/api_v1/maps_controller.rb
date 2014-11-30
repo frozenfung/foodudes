02fa8e5627bb3c0310cd4129fd120927adabf5f1
frozenfung <frozenfung@gmail.com>
2014-11-30 15:36:26 +0800

    files for last commute

---
+++ app/controllers/api_v1/maps_controller.rb
@@ -0,0 +1,41 @@
+class ApiV1::MapsController < ApiController
+  def index
+    mobile_user = mobile_user(params[:mobile_id])
+    if mobile_user
+      map_infos = []
+      user_infos = []
+      friends = mobile_user.friends
+      friends.each do |friend|
+        friend.restaurants.each do |restaurant|
+          # Save map markers data
+          map_info = {}
+          map_info['id'] = restaurant.id
+          map_info['name'] = restaurant.name
+          map_info['phone_number'] = restaurant.phone_number
+          map_info['address'] = restaurant.address
+          map_info['marker_lng'] = restaurant.lng
+          map_info['marker_lat'] = restaurant.lat
+          map_info['user_id'] = []
+          restaurant.users.each do |user|
+            map_info['user_id'] << user.id
+          end
+          map_infos << map_info
+        end
+        # Save users data
+        user_info = {}
+        user_info['id'] = friend.id
+        user_info['name'] = friend.name
+        user_info['email'] = friend.email
+        user_info['image'] = friend.image
+        user_infos << user_info
+      end
+      recommend_infos = {}
+      recommend_infos['restaurants'] = map_infos
+      recommend_infos['users'] = user_infos
+      respond_to do |format|
+        format.html
+        format.json { render json: recommend_infos.as_json() }
+      end
+    end
+  end
+end
