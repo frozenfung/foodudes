5e2316007a2aee9eaa525b96630ee5ceca4cdb42
frozenfung <frozenfung@gmail.com>
2014-12-02 10:28:55 +0800

    setting local_varible.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '795450763826632', '04ee62e240c66a00236d11539e5bfe65',
+  provider :facebook, ENV['FB_KEY'], ENV['SECRET'],
              :scope => 'email, user_friends', :display => 'popup'
-  # provider :facebook, '472910486181357', 'd2f0fa264e1edcf98278e01b4cdd09f0',
-  #            :scope => 'email, user_friends', :display => 'popup'
 end
