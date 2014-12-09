7b060c39d14e71dd5bf747d061f5e4a5a3841967
frozenfung <frozenfung@gmail.com>
2014-12-09 10:31:02 +0800

    build recommend function for mobile API

--- app/controllers/api_v1/restaurants_controller.rb
+++ app/controllers/api_v1/restaurants_controller.rb
@@ -2,15 +2,16 @@ class ApiV1::RestaurantsController < ApiController
   def recommend
     mobile_user = mobile_user(params[:mobile_id])
     if mobile_user
-      id = params[:id]
-      content = params[:content]
-      restaurant = Restaurant.where(:id => id).first
-      recommend = mobile_user.recommends.new
-      recommend.restaurant = restaurant
-      recommend.content = content
-      recommend.save!
-
-      render :text => 'OK'
+      modified_params = {
+        :name => params[:name],
+        :phone_number => params[:phone_number],
+        :address => params[:address],
+        :lat => params[:lat],
+        :lng => params[:lng]
+      }
+      restaurant = Restaurant.find_or_create_from_form(modified_params)
+      Recommend.new_recommend(mobile_user, restaurant, params[:content])
+      render :text => 'Recommend Success!'
     end
   end
 end
--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,4 +1,12 @@
 class Recommend < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+
+  def self.new_recommend(user, restaurant, content)
+    recommend = Recommend.new
+    recommend.user = user
+    recommend.restaurant = restaurant
+    recommend.content = content
+    recommend.save!
+  end
 end
