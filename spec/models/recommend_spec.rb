d4407055bb701de6839cb4a3dc1c6b43ce10396f
frozenfung <frozenfung@gmail.com>
2014-12-10 10:33:16 +0800

    rspec for recommend and restaurant model

--- spec/models/recommend_spec.rb
+++ spec/models/recommend_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
-
-# describe Recommend do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Recommend, :type => :model do
+  before do
+    @user = User.create!( :name => 'fung' )
+    @restaurant = Restaurant.create!( :name => '阿妹麵店' )
+  end
+  let(:params){
+    { :content => '推薦肉羹湯\n乾意麵搭配燙青菜\n讚拉' }
+  }
+  it "should build a new recommend" do
+    expect(Recommend.count).to eq(0)
+    Recommend.new_recommend(@user, @restaurant, params)
+    expect(Recommend.last.user.name).to eq('fung')
+    expect(Recommend.last.restaurant.name).to eq('阿妹麵店')
+    expect(Recommend.last.content).to eq('推薦肉羹湯\n乾意麵搭配燙青菜\n讚拉')
+  end
+end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,18 +1,49 @@
 require 'spec_helper'
 RSpec.describe Restaurant, :type => :model do
+  describe "find_or_create_from_form" do
+    before do
+      @restaurant = Restaurant.create!(
+        :name => '阿妹麵店', 
+        :phone_number => '111111111',
+        :address => '台北市中正區羅斯福路1號之1',
+        :lng => 1.2, 
+        :lat => 3.4
+      )
+    end
 
-  let(:params){ 
-    {:name => 'test',
-     # name: 'test' 
-     :address => 'test', 
-     :lng => 0.1,
-     :lat => 0.1
+    let(:params_find){ 
+      {
+       :name => '阿妹麵店',
+       :phone_number => '123456789',
+       :address => '台北市中正區羅斯福路1號', 
+       :lng => 1.2,
+       :lat => 3.4
+      }
     }
-  }
 
-  it "should find or create a restaurant" do
-    Restaurant.find_or_create_from_form(params)
-    # expect(param).to eq('test')
-    expect(Restaurant.where(:name => 'test').first.address).to eq('test')
+    let(:params_create){ 
+      {
+       :name => '威爾貝克咖啡',
+       :phone_number => '987654321',
+       :address => '台北市中正區羅斯福路2號', 
+       :lng => 5.6,
+       :lat => 7.8
+      }
+    }
+
+    it "should find a restaurant from form" do
+      expect(@restaurant.address).to eq('台北市中正區羅斯福路1號之1')
+      restaurant = Restaurant.find_or_create_from_form(params_find)
+      restaurant = Restaurant.where(:name => '阿妹麵店').first
+      expect(restaurant.address).to eq('台北市中正區羅斯福路1號')
+    end
+
+    it "should create a restaurant from form" do
+      restaurant = Restaurant.where(:name => '威爾貝克咖啡').first
+      expect(restaurant).to eq(nil)
+      restaurant = Restaurant.find_or_create_from_form(params_create)
+      restaurant = Restaurant.where(:name => '威爾貝克咖啡').first
+      expect(restaurant.address).to eq('台北市中正區羅斯福路2號')      
+    end
   end
 end
