24d687bc48fe0d012dba28a7547ae68ad855a28a
frozenfung <frozenfung@gmail.com>
2015-05-17 23:34:12 +0800

    Set serve static assets to true

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ module Foodudes
   class Application < Rails::Application
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
