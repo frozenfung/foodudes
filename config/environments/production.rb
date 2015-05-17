0b23712a643dd190e2d6362ca976d663003f6305
frozenfung <frozenfung@gmail.com>
2015-05-18 07:17:56 +0800

    Set log configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
+  config.logger = Logger.new(STDOUT)
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
